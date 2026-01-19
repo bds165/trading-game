a19a02e7f0dff8fa9f0ac102d64453f38d6c8561
bds165 <samir_abbad@hotmail.com>
2026-01-19 16:57:13 +1300

    cloning

--- engine.py
+++ engine.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, field
 from idlelib.editor import darwin
-from typing import List, Dict
+from typing import List, Dict, Tuple
 import random
 
 @dataclass
@@ -18,6 +18,28 @@ class Company:
     def liquidation_value(self) -> int:
         total = sum(c.value for c in self.cards)
         return max(total,0)
+
+def generate_sar_report(companies: Dict[str, Company]) -> Dict[str, int]:
+    reports:Dict[str, int] = {}
+    for name, company in companies.items():
+        sample_cards = random.sample(company.cards, k=5)
+        sar_value = sum(c.value for c in sample_cards) + 50 #(50 is the constant)
+        reports[name] = sar_value
+    return reports
+
+def apply_eps_random_company (companies: Dict[str, Company]) -> Tuple[str, int]:
+    name = random.choice(list(companies.keys()))
+    company = companies[name]
+    old_value = company.liquidation_value
+    idx = random.randrange(len(company.cards))
+    company.cards[idx] = make_random_card()
+    new_value = company.liquidation_value
+    delta = new_value - old_value
+    return name, delta
+
+def peek_cards(company: Company, k: int =3) -> List[int]:
+    sample_cards = random.choices(company.cards, k=k)
+    return [c.value for c in sample_cards]
 
 def make_random_card() -> Card:
     ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
